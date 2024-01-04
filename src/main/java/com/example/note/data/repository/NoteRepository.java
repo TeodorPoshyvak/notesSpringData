e7c971d3df5fb1ed797c5d22de4ab073dbf09ea6
Teodor Poshyvak <132757783+TeodorPoshyvak@users.noreply.github.com>
2024-01-04 21:26:03 +0200

    Update NoteRepository.java

--- src/main/java/com/example/note/data/repository/NoteRepository.java
+++ src/main/java/com/example/note/data/repository/NoteRepository.java
@@ -2,7 +2,9 @@ package com.example.note.data.repository;
 
 import com.example.note.data.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
 }
